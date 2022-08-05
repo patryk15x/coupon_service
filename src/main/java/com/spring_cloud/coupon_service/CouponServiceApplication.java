34469c78806c8fd35c02947a231e7f231907b316
Patryk Szaruga <szarugapatryk@gmail.com>
2022-08-05 18:55:12 +0200

    Initial commit

---
+++ src/main/java/com/spring_cloud/coupon_service/CouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring_cloud.coupon_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponServiceApplication.class, args);
+    }
+
+}
