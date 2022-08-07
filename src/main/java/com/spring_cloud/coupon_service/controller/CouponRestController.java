0ef1d64e3d4f002d2b2260e2c1bb4eb8d23815bf
Patryk Szaruga <szarugapatryk@gmail.com>
2022-08-07 16:15:41 +0200

    add Coupon controller

---
+++ src/main/java/com/spring_cloud/coupon_service/controller/CouponRestController.java
@@ -0,0 +1,24 @@
+package com.spring_cloud.coupon_service.controller;
+
+import com.spring_cloud.coupon_service.model.Coupon;
+import com.spring_cloud.coupon_service.repo.CouponRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/coupon-api")
+@RequiredArgsConstructor
+public class CouponRestController {
+
+    private final CouponRepo repo;
+
+    @PostMapping("/coupons")
+    public Coupon create(Coupon coupon){
+        return repo.save(coupon);
+    }
+
+    @GetMapping("/coupons/{code}")
+    public Coupon getCouponByCode(@PathVariable String code){
+        return repo.findByCode(code);
+    }
+}
